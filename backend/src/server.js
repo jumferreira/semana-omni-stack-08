223a4b023b7c07aa4dec9331f9cda26f9c23bbff
Juliana Ferreira <juju_smf@hotmail.com>
2019-11-16 20:06:05 -0300

    Video 5 - atividade finalizada
    Criei uma conexão de socket em tempo real com o node de o react e o node e o react-native

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,19 +9,44 @@ const cors = require('cors');
 const routes = require('./routes');
 
 // servidor criado
-const server = express('');
+const app = express('');
+// to criando um server com webSocket pra conseguir criar ações em tempo real
+// e conectado com o front. Mas pra isso, eu conecto com o server que eu já tenho
+// que agora chama app
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+// cada vez que alguém se conectar eu recebo o socket
+// socket é a conexão do backend e do frontend
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+    // preciso armazenar o id do socket
+    connectedUsers[user] = socket.id;
+});
+
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-niiul.mongodb.net/omnistack8?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
+// criado um middleware pra interceptar as requisições
+app.use((req, res, next) => {
+    // vou estar mandando os dados pro controller
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
 // permite o react de consumir os dados do backend
-server.use(cors());
+app.use(cors());
 // pro servidor saber que o retorno das requisições vai ser em json
-server.use(express.json());
+app.use(express.json());
 
 // usa esse use para declarar que quer usar algo novo no projeto (modulo, plugin, ...)
-server.use(routes);
+app.use(routes);
 
 // porta que vai ser escutada pelo servidor criada
 server.listen(3333);
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,17 +1,20 @@
 // função que recebe 2 parametros: função que eu quero executar e quando eu quero executar
 import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import { Link } from 'react-router-dom';
 import './Main.css';
 
-import { Link } from 'react-router-dom';
 import api from '../services/api';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 // toda vez que o id for alterado, a gente vai chamar essa função
 export default function Main({ match }) {
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
         async function loadUser () {
@@ -25,6 +28,16 @@ export default function Main({ match }) {
         }
 
         loadUser();
+    }, [match.params.id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id }
+        });
+
+        socket.on('match', dev => {
+            console.log();
+        });
     }, [match.params.id]);
 
     async function handleLike(id) {
@@ -72,6 +85,17 @@ export default function Main({ match }) {
                 </ul>
             ) : (
                 <div className="empty">Acabou :(</div>
+            ) }
+
+            { matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="It's a match" />
+                    <img className="avatar" src={matchDev.avatar} alt="It's a match" />
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+
+                    <button type="button" onClick={() => setMatchDev(null)}>FECHAR</button>
+                </div>
             ) }
         </div>
     );
